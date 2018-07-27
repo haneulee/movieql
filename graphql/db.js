2a8ecc7faba185a784e00186e9a371f0d3261a35
Haneul Lee <lovesky4294@gmail.com>
2018-07-27 10:11:18 +0900

    added getMovies limit, rating

--- graphql/db.js
+++ graphql/db.js
@@ -1,7 +1,18 @@
 import fetch from "node-fetch";
-const MOVIE_URL = "https://yts.am/api/v2/list_movies.json";
+const MOVIE_URL = "https://yts.am/api/v2/list_movies.json?";
 
-export const getMovies = (limit, rating) =>
-  fetch(`${MOVIE_URL}`)
+export const getMovies = (limit, rating) => {
+  let REQUEST_URL = MOVIE_URL;
+
+  if (limit > 0) {
+    REQUEST_URL += `limit=${limit}`;
+  }
+
+  if (rating > 0) {
+    REQUEST_URL += `&minimum_rating=${rating}`;
+  }
+
+  return fetch(`${REQUEST_URL}`)
     .then(res => res.json())
     .then(json => json.data.movies);
+};
